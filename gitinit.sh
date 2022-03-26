e27254b3910590a78efde82ce8299aeff312a4d3
Ivan Sprut <i.sprutfromua@gmail.com>
2022-03-26 23:57:06 +0200

    Zero commit

---
+++ gitinit.sh
@@ -0,0 +1,8 @@
+git init
+git add .
+git config --global user.email "i.sprutfromua@gmail.com"
+git config --global user.name "Ivan Sprut"
+git commit -m "Zero commit"
+git branch -M main
+git remote add origin git@github.com:isprutfromua/rest-countries-api-with-color-theme-switcher.git
+git push -u origin main
